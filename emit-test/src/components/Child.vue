85c87a70f840693a872e28e4c07bf1061954da7e
HyunHo Lee <dlgusgh2001@naver.com>
2022-10-22 23:12:22 +0900

    :sparkles: parent child emit
    typescript

---
+++ emit-test/src/components/Child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="child">
+    <button @click="handleClick">{{ isClick }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private isClick?: boolean;
+  @Emit("handle-click")
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  handleClick() {}
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ emit-test/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="parent">
+    <Child :isClick="isClick" @handle-click="handleClick" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Child from "@/components/Child.vue";
+
+@Component({
+  components: {
+    Child,
+  },
+})
+export default class HomeView extends Vue {
+  isClick = false;
+
+  handleClick() {
+    this.isClick = !this.isClick;
+  }
+}
+</script>
