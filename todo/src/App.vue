5d43bf82b9c73e028ba32987aa831d92fa60353b
HyunHo Lee <dlgusgh2001@naver.com>
2022-10-24 16:53:09 +0900

    :tada: Todo (JavaScript)
    입력하 input값에 따른 todo Item 컴포넌트 추가

---
+++ todo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">TODO</h1>
+    <input
+      type="text"
+      class="w-100 p-2"
+      placeholder="할일을 입력하세요"
+      v-model="todoInput"
+      @keyup.enter="addTodoItem"
+    />
+    <TodoItem
+      v-for="todoItem in todoItems"
+      :key="todoItem.id"
+      :todoItem="todoItem"
+    />
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem.vue";
+
+export default {
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      todoInput: "",
+      todoItems: [
+        {
+          id: 1,
+          text: "Vue2 기초 공부",
+          checked: false,
+        },
+        {
+          id: 2,
+          text: "Vue2 + Typescript 공부",
+          checked: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodoItem(e) {
+      this.todoItems.push({
+        id: this.todoItems.length + 1,
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoInput = "";
+    },
+  },
+};
+</script>
---
+++ todo/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-3">
+    <input type="checkbox" :checked="todoItem.checked" />
+    <span class="m-3">{{ todoItem.text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todoItem: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
