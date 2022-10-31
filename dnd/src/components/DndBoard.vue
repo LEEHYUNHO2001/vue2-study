f2866561eed8c3c768a5a61803322c17496ed5a3
HyunHo Lee <dlgusgh2001@naver.com>
2022-10-31 16:10:07 +0900

    :sparkles: drag and drop 기능 구현

---
+++ dnd/src/components/DndBoard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="board-container">
+    <h1>{{ msg }}</h1>
+    <article class="boarad">
+      <div
+        class="box"
+        @mousemove="doDragItem"
+        @mousedown="startDragItem"
+        @mouseup="stopDragItem"
+        :style="customItemStyle"
+      />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DndBoard",
+  data() {
+    return {
+      msg: "DND",
+      box: {
+        x: 100,
+        y: 200,
+        clickX: 0,
+        clickY: 0,
+        dragging: false,
+      },
+    };
+  },
+  methods: {
+    boardSize() {},
+    doDragItem(e) {
+      if (this.box.dragging) {
+        this.box.x = e.clientX;
+        this.box.y = e.clientY;
+      }
+    },
+    startDragItem() {
+      this.box.dragging = true;
+    },
+    stopDragItem() {
+      this.box.dragging = false;
+    },
+  },
+  computed: {
+    customItemStyle() {
+      return {
+        top: `${this.box.y}px`,
+        left: `${this.box.x}px`,
+        border: this.box.dragging ? "3px solid deeppink" : "3px solid #fff",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.boarad {
+  widows: 90vw;
+  height: 50vh;
+  border: 3px solid #000;
+  border-radius: 10px;
+  background-color: #dbdbdb;
+}
+
+.box {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: #fff;
+}
+</style>
