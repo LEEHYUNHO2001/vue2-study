9c6c3270272cbd5f1c83d56bf6b33a6e0515c619
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2022-11-03 14:11:29 +0900

    :fire: 과거 코드 제거

--- phone-number/src/components/PhoneNumber.vue
+++ phone-number/src/components/PhoneNumber.vue
@@ -1,15 +1,13 @@
 <template>
 	<section class="phone-number">
 		<h1 class="title">{{ title }}</h1>
-		<InputUserForm @addUser="addUser" />
-		<UserList :userList="userList" />
+		<InputUserForm />
+		<UserList />
 	</section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
-import { User } from '@/types';
 
 import InputUserForm from './InputUserForm.vue';
 import UserList from './UserList.vue';
@@ -22,18 +20,6 @@ import UserList from './UserList.vue';
 })
 export default class PhoneNumber extends Vue {
 	title = '전화번호부';
-	userList = [
-		{
-			name: 'example user',
-			phoneNumber: '01012345678',
-			email: 'ex1@iga.com',
-			date: '2022년 3월 20일 14시 21분',
-		},
-	] as User[];
-
-	addUser(user: User) {
-		this.userList.push(user);
-	}
 }
 </script>
 
