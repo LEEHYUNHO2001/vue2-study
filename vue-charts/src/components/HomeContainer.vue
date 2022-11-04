9aaf543483956c6948b9d5551f1a9667d9270388
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2022-11-04 12:16:45 +0900

    :tada: vue2 vuex ts

---
+++ vue-charts/src/components/HomeContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomeContainer extends Vue {
+  title = "HomeContainer";
+}
+</script>
+
+<style scoped></style>
---
+++ vue-charts/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HomeContainer.vue";
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
