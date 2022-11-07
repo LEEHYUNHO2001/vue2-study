b64185ef38a5b421ccde305ce20887e87d615b1d
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2022-11-07 17:23:52 +0900

    :tada: vue2 class ts scss

---
+++ vue-scss/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <header>
+      <h1 class="title">{{ msg }}</h1>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.title {
+  font-size: 26px;
+  font-weight: 700;
+  color: $red;
+}
+</style>
