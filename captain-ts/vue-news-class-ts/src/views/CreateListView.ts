0ae41eb950818d55a7348d06dd6405f0d72796df
HyunHo Lee <dlgusgh2001@naver.com>
2022-11-22 22:33:16 +0900

    create list view type

--- captain-ts/vue-news-class-ts/src/views/CreateListView.ts
+++ captain-ts/vue-news-class-ts/src/views/CreateListView.ts
@@ -1,13 +1,15 @@
+import { CreateElement } from "vue/types/umd";
+
 import ListView from "./ListView.vue";
 import bus from "../utils/bus";
 
-export default function createListView(name) {
+export default function createListView(name: string) {
   return {
     name,
     mounted() {
       bus.$emit("off:progress");
     },
-    render(h) {
+    render(h: CreateElement) {
       return h(ListView);
     },
   };
