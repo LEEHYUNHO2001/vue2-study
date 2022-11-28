b5d8ff8386d58d64cdb8f7e7ddc1e7076e19fe14
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2022-11-28 16:42:16 +0900

    authorization bearer

--- phone-number/src/router/index.ts
+++ phone-number/src/router/index.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
+import axios from "axios";
+import { apiEndpoint } from "@/constants";
 
 Vue.use(VueRouter);
 
@@ -10,6 +12,7 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "home",
     component: HomeView,
+    meta: { authRequired: true },
   },
   {
     path: "/login",
@@ -24,6 +27,23 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async function (to, from, next) {
+  if (to.matched.some((routeInfo) => routeInfo.meta.authRequired)) {
+    try {
+      const accessToken = router.app.$store.getters.getAccessToken;
+      const authHeader = { Authorization: `Bearer ${accessToken}` };
+      const res = await axios.get(`${apiEndpoint}/api/Login/GetList`, {
+        headers: authHeader,
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
--- phone-number/src/store/modules/login/login.ts
+++ phone-number/src/store/modules/login/login.ts
@@ -1,7 +1,9 @@
 import { loginState as state } from "./state";
 import * as mutations from "./mutations";
+import * as getters from "./getters";
 
 export default {
   state,
   mutations,
+  getters,
 };
