1d07a2656a3fe402e5ec66f848a838a15232fa03
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2022-11-28 15:08:13 +0900

    form handle and get access token

--- phone-number/src/components/Login.vue
+++ phone-number/src/components/Login.vue
@@ -1,21 +1,78 @@
 <template>
-  <div>
+  <form @submit.prevent="handleSubmit">
     <div>
-      <label for="userName">userName :</label>
-      <input type="text" id="userName">
+      <label for="userName">Name :</label>
+      <input
+        type="text"
+        id="userName"
+        name="userName"
+        placeholder="userName"
+        @input="handleLoginData"
+        :value="loginData.userName"
+      />
     </div>
     <div>
-      <label for="password">password :</label>
-      <input type="password" id="password">
+      <label for="password">Password :</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="password"
+        @input="handleLoginData"
+        :value="loginData.password"
+      />
     </div>
-  </div>
+    <button type="submit">Login</button>
+  </form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+import { apiEndpoint } from "@/constants";
+
+interface LoginData {
+  userName: string;
+  password: string;
+}
 
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  loginData = {
+    userName: "",
+    password: "",
+  } as LoginData;
+
+  async handleSubmit() {
+    await this.postAccessToken();
+    await this.clearLoginData();
+  }
+  handleLoginData(e: InputEvent) {
+    const eTarget = e.target as HTMLInputElement;
+    this.loginData = {
+      ...this.loginData,
+      [eTarget.name]: eTarget.value,
+    };
+  }
+  clearLoginData() {
+    this.loginData = {
+      userName: "",
+      password: "",
+    }
+  }
+  async postAccessToken() {
+    try {
+      const res = await axios({
+        url: `${apiEndpoint}/api/Login/GetToken`,
+        method: "post",
+        data: this.loginData,
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
 </script>
 
 <style scoped></style>
